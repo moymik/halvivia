27c8c721afcfdf6714bd085ca37ffef133ce42eb
Artem Kim <6aremkim@gmail.com>
2026-05-26 17:02:29 +0300

    chore: add dependendcies for images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      new URL('https://i.pinimg.com/**'),
+      new URL(`https://${process.env.NEXT_PUBLIC_BLOB_ID}.public.blob.vercel-storage.com/**`),
+    ],
+  },
   /* config options here */
 };
 
