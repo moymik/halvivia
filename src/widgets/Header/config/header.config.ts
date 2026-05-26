7ec5b4e24822df04aa0e734a1159d8d69efb62ed
Artem Kim <6aremkim@gmail.com>
2026-05-26 21:14:11 +0300

    chore: refactor

---
+++ src/widgets/Header/config/header.config.ts
@@ -0,0 +1,6 @@
+import { type NavigationLink, NAVIGATION_LINKS } from 'src/shared/config';
+
+export const headerTabLinks = [
+  NAVIGATION_LINKS.cinema,
+  NAVIGATION_LINKS.library,
+] satisfies readonly NavigationLink[];
