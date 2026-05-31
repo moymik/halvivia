3a5a5e90d7513f0cbf503def8b4560cadf580b05
Artem Kim <6aremkim@gmail.com>
2026-05-31 05:09:30 +0300

    feat: complete register flow

---
+++ src/pages/auth/login/LoginPage.tsx
@@ -0,0 +1,7 @@
+//export type LoginPageProps = {}
+
+export const LoginPage = () => {
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
---
+++ src/shared/lib/auth/index.ts
@@ -0,0 +1,1 @@
+export { verifySession } from './dal';
